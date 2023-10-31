d9eeac0111f58820b6de4b29b8758de3faefcd4d
Richard <forbannade@gmail.com>
2023-10-31 12:40:36 +0100

    Removed css

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,23 @@
-
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import Main from '@/components/Main';
-import './globals.css'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Main from "@/components/Main";
 //import Link from 'next/link';
 
 export const metadata = {
-  title: 'Next-DMS',
-  description: 'Next Data Management System',
-}
+  title: "Next-DMS",
+  description: "Next Data Management System",
+};
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="en">
-      
       <body>
-      <Header/>
-    
-      <Main>
-        {children}
-      </Main>
+        <Header />
 
-      <Footer/>
+        <Main>{children}</Main>
+
+        <Footer />
       </body>
-
     </html>
-
-  )
+  );
 }
