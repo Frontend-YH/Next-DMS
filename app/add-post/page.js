f2d018078232f3b340a3b7fb10d1a059c2a65de5
Anton Jonsson <94antjon@skola.boras.se>
2023-11-01 09:20:53 +0100

    removed junk code

--- app/add-post/page.js
+++ app/add-post/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import QEditor from "@/components/QEditor";
 import ReactQuill from "react-quill";
